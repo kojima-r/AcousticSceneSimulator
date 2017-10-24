eb58f5036a6ea1a5bb6abba8a41a044ae26afc16
kojima <kojima@cyb.mei.titech.ac.jp>
2017-10-24 22:24:22 +0900

    add setting 3d

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 git clone --recursive https://github.com/naegawa/SimMch.git
 mkdir -p data
 mkdir -p data_sim
+mkdir -p data_sim_3d
+mkdir -p label
+mkdir -p label_3d
+
 #python build_sim_dataset.py ./tf/tamago_rectf.zip
 
